ae80c5554850094cb9d3743402fb8eb789d46e9a
pardub <42610272+pardub@users.noreply.github.com>
2024-09-05 14:34:16 +0000

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -1,1 +1,33 @@
+#!/bin/bash -xev
 
+mkdir -p ~/.ssh/config
+
+# +---------+
+# | Install |
+# +---------+
+
+# Tools for Emacs compiling
+sudo add-apt-repository -y  ppa:ubuntu-toolchain-r/ppa
+
+sudo apt update -y
+sudo apt upgrade -y
+echo "$USER ALL=(ALL:ALL) ALL" | sudo EDITOR='tee -a' visudo
+echo "Defaults:$USER timestamp_timeout=120" | sudo EDITOR='tee -a' visudo
+
+# Add dns from https://dns.watch/
+sudo echo 'nameserver 84.200.69.80' | sudo tee -a /etc/resolv.conf
+sudo echo 'nameserver 84.200.70.40' | sudo tee -a /etc/resolv.conf
+
+mkdir -p "$HOME"/.config/zsh
+cd || exit
+
+# Install automatically security updates
+sudo apt install unattended-upgrades -y
+
+# +--------------------+
+# | Python virtual Env |
+# +--------------------+
+
+sudo apt install python3-venv python3-pip -y
+python3 -m venv global_env
+source global_env/bin/activate

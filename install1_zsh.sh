1f063fe12dfe97d6b757c3f49d6a9e2d0ed4b3ba
pardub <42610272+pardub@users.noreply.github.com>
2024-09-05 14:51:04 +0000

    Update install1_zsh.sh

--- install1_zsh.sh
+++ install1_zsh.sh
@@ -2,7 +2,9 @@
 #!/bin/bash -xev
 
 mkdir -p "$HOME"/.config/zsh
-mkdir -p "$HOME"/.config/nvim 
+cd || exit
+mkdir -p "$HOME"/.config/nvim
+cd || exit
 
 sudo apt install -y zsh
 sudo apt install -y  util-linux

9d7e9f690a6765a7a4560262809e86f91a9559e0
pardub <42610272+pardub@users.noreply.github.com>
2024-09-05 14:27:31 +0000

    Update install1.sh

--- install1.sh
+++ install1.sh
@@ -11,35 +11,3 @@ sudo apt install -y  util-linux
 # sudo chsh -s /bin/zsh marc ## replaced by the command below
 sudo chsh -s $(which zsh) $USER
 
-cd $HOME
-if [ -e "$HOME"/.zshenv ]
-then rm "$HOME"/.zshenv
-fi
-
-if [ ! -e "$HOME"/.cache/zsh/history ]
-then 
-mkdir -p "$HOME"/.cache/zsh || exit
-touch "$HOME"/.cache/zsh/history
-fi
-
-# Download .zshenv
-cd "$HOME" || exit
-wget https://raw.githubusercontent.com/pardub/dotfiles/main/dotfiles/.zshenv
-
-# Download .zshrc
-cd "$HOME"/.config/zsh || exit
-wget https://raw.githubusercontent.com/pardub/dotfiles/main/dotfiles/.zshrc
-
-# Download .aliases
-cd "$HOME"/.config/zsh || exit
-wget https://raw.githubusercontent.com/pardub/dotfiles/main/dotfiles/.aliases
-
-# Download zsh-functions
-cd "$HOME"/.config/zsh || exit
-wget https://raw.githubusercontent.com/pardub/dotfiles/main/dotfiles/zsh-functions
-
-# Download init.vim
-cd "$HOME"/.config/nvim || exit
-wget https://raw.githubusercontent.com/pardub/dotfiles/main/dotfiles/init.vim
-
-sudo reboot

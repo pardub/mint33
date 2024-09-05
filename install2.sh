c00b695f90478efd0ed4c09de4cf93bff1ca080d
pardub <42610272+pardub@users.noreply.github.com>
2024-09-05 14:50:50 +0000

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -18,9 +18,6 @@ echo "Defaults:$USER timestamp_timeout=120" | sudo EDITOR='tee -a' visudo
 sudo echo 'nameserver 84.200.69.80' | sudo tee -a /etc/resolv.conf
 sudo echo 'nameserver 84.200.70.40' | sudo tee -a /etc/resolv.conf
 
-mkdir -p "$HOME"/.config/zsh
-cd || exit
-
 # Install automatically security updates
 sudo apt install unattended-upgrades -y
 

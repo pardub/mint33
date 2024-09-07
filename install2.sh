ae8b2082170893e873d6961cc53ad37397e818de
pardub <42610272+pardub@users.noreply.github.com>
2024-09-07 08:40:12 +0000

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -21,17 +21,13 @@ sudo echo 'nameserver 84.200.70.40' | sudo tee -a /etc/resolv.conf
 # Install automatically security updates
 sudo apt install unattended-upgrades -y
 
-
 # INSTALL JETBRAINS MONO FONTS IN ~/.local/share/fonts
 mkdir -p "$HOME"/.local/share/fonts/jetbrains
 cd /tmp
-sudo wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.2/JetBrainsMono.zip
-#sudo wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/JetBrainsMono.zip
-#sudo wget https://github.com/ryanoasis/nerd-fonts/releases/download/2.2.0-RC/JetBrainsMono.zip
+sudo wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/JetBrainsMono.zip
 sudo unzip /tmp/JetBrainsMono.zip -d ~/.local/share/fonts/jetbrains
 fc-cache -v
 cd || exit
-
 
 # +--------------------+
 # | Python virtual Env |
